a85d11c718bdda36279e92bb9e63e894008b44ca
Jacob <typefortyproductions@gmail.com>
2020-08-11 13:13:51 -0400

    added button to uitest

--- uitest.py
+++ uitest.py
@@ -1,9 +1,14 @@
 import tkinter as tk
 
+def display_stuff():
+    print("Hello World!")
+
 window = tk.Tk()
 label = tk.Label(text="Python rocks!")
 entry = tk.Entry(text="Whats up?")
+button = tk.Button( window, text="Push Me!", command = display_stuff )
 label.pack()
 entry.pack()
+button.pack()
 
 window.mainloop()
