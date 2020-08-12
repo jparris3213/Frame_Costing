89e3c79e1fbf568ffdfb8cca691a687a32c96ffd
Jacob Parris <you@example.com>
2020-08-11 22:41:47 -0400

    Created Preliminary results of UI Testing

--- uitest.py
+++ uitest.py
@@ -1,14 +1,17 @@
 import tkinter as tk
+import time
 
 def display_stuff():
-    print("Hello World!")
+    label.configure(text = entry.get())
 
 window = tk.Tk()
-label = tk.Label(text="Python rocks!")
-entry = tk.Entry(text="Whats up?")
-button = tk.Button( window, text="Push Me!", command = display_stuff )
+label = tk.Label(text = "Test")
 label.pack()
+
+entry = tk.Entry()
 entry.pack()
+
+button = tk.Button( window, text="Push Me!", command = display_stuff )
 button.pack()
 
 window.mainloop()
