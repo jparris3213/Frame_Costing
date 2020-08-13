d16994bc9a2976d6a2ab4676b88eaf9de92aefe9
Jacob Parris <you@example.com>
2020-08-13 17:53:36 -0400

    Create: RediFrame UI Change:UItest to grid layout

---
+++ rediframeui.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from tkinter import ttk
+import time
+
+
+main = tk.Tk()
+
+main.geometry("800x600")
+
+
+#TODO: Declare Variables present on page, starting with Plywood
+
+#TODO: Admin Area: Number, Name, Customer, Current Price, New Price, MATLAB
+
+#TODO: Plywood Yield Calculator
+
+#TODO: Hardwood Entry Table
+
+#TODO: Labor Entry
+
+#TODO: Spring Up Information Box
+
+#TODO: Varibles for: Plywood, Hardwood, KD/Assembled
+
+#TODO: Create layout manager for grid layout in tkinter
+
--- uitest.py
+++ uitest.py
@@ -5,13 +5,14 @@ def display_stuff():
     label.configure(text = entry.get())
 
 window = tk.Tk()
+window.geometry("800x600")
 label = tk.Label(text = "Test")
-label.pack()
+label.grid(row = 0, column = 1)
 
 entry = tk.Entry()
-entry.pack()
+entry.grid(row = 0, column = 2)
 
 button = tk.Button( window, text="Push Me!", command = display_stuff )
-button.pack()
+button.grid(row = 0, column = 3)
 
 window.mainloop()
