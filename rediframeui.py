ae359a437473c90769eb9dfa881e48d8c7a0c532
Jacob Parris <you@example.com>
2020-08-13 18:04:22 -0400

    First Part of Plywood Entry implemented

--- rediframeui.py
+++ rediframeui.py
@@ -14,6 +14,16 @@ main.geometry("800x600")
 
 #TODO: Plywood Yield Calculator
 
+plywood_header_1 = tk.Label(text = "Qty")
+plywood_header_2 = tk.Label(text = "Whole Sheets")
+plywood_header_3 = tk.Label(text = "Partial Sheets")
+plywood_header_4 = tk.Label(text = "/96 or /48")
+
+qty_1_label = tk.Label(text = "One")
+qty_1_whole = tk.Entry()
+qty_1_partial = tk.Entry()
+qty_1_over = tk.Entry()
+
 #TODO: Hardwood Entry Table
 
 #TODO: Labor Entry
@@ -24,3 +34,14 @@ main.geometry("800x600")
 
 #TODO: Create layout manager for grid layout in tkinter
 
+plywood_header_1.grid(row = 0 , column = 0)
+plywood_header_2.grid(row = 0 , column = 1)
+plywood_header_3.grid(row = 0 , column = 2)
+plywood_header_4.grid(row = 0 , column = 3)
+
+qty_1_label.grid(row = 1, column = 0)
+qty_1_whole.grid(row = 1, column = 1)
+qty_1_partial.grid(row = 1, column = 2)
+qty_1_over.grid(row = 1, column =3)
+
+main.mainloop()
